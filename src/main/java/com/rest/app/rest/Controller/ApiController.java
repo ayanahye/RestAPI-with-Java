493e0ca0fdf6d191b612ceabbc42af1f81e3c0a3
ayana <ayanahye2@gmail.com>
2024-01-06 14:29:58 -0800

    Added get, post, put and delete requests.

--- src/main/java/com/rest/app/rest/Controller/ApiController.java
+++ src/main/java/com/rest/app/rest/Controller/ApiController.java
@@ -1,13 +1,52 @@
 package com.rest.app.rest.Controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.rest.app.rest.Models.User;
+import com.rest.app.rest.Repo.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class ApiController {
 
+    // autowire handles dependency injections
+    @Autowired
+    private UserRepo userRepo;
+
     @GetMapping(value = "/")
     public String getPage() {
         return "Welcome";
+    }
+
+    // jpa has pre built methods like getting inv and lists of objects from db
+    @GetMapping(value="/users")
+    public List<User> getUsers() {
+        return userRepo.findAll();
+    }
+
+    @PostMapping(value="/save")
+    public String saveUser(@RequestBody User user) {
+        userRepo.save(user);
+        return "Saved...";
+    }
+
+    // issue that when the server restarts the update and delete queries do not work
+    @PutMapping(value = "/update/{id}")
+    public String updateUser(@PathVariable long id, @RequestBody User user) {
+        // find the user and get it
+        User updatedUser = userRepo.findById(id).get();
+        // set the username and password to the ones in the object class
+        updatedUser.setUsername(user.getUsername());
+        updatedUser.setPassword(user.getPassword());
+        userRepo.save(updatedUser);
+        return "Updated...";
+    }
+
+    @DeleteMapping(value = "/delete/{id}")
+    public String deleteUser(@PathVariable long id) {
+        User deleteUser = userRepo.findById(id).get();
+        userRepo.delete(deleteUser);
+        return "Deleted user with id: " + id;
     }
 }
